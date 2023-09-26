0a837adfdd00a71e6a9b7630eb2f73ea43f70372
Anericokakai <challykiddoh@gmail.com>
2023-09-26 18:01:40 +0300

    initial spring boot project

---
+++ src/main/java/com/springSecurity/springSecurity_second/SpringSecuritySecondApplication.java
@@ -0,0 +1,13 @@
+package com.springSecurity.springSecurity_second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySecondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springSecurity/springSecurity_second/SpringSecuritySecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springSecurity.springSecurity_second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
